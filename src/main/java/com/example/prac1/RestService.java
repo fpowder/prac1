c51ceded250dacd067b48d09de6922b15c611a92
kneelick <fpowder1@gmail.com>
2020-07-28 15:11:22 +0900

    html setting

--- src/main/java/com/example/prac1/RestService.java
+++ src/main/java/com/example/prac1/RestService.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,6 +18,16 @@ public class RestService {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/books")
+    public Map<String, String> getBooks(){
+
+        Map<String, String> books = new HashMap<String, String>();
+        books.put("1","book1");
+        books.put("2", "book2");
+
+        return books;
     }
 
 }
--- src/main/java/com/example/prac1/dto/Greeting.java
+++ src/main/java/com/example/prac1/dto/Greeting.java
@@ -1,32 +1,14 @@
 package com.example.prac1.dto;
 
 public class Greeting {
-<<<<<<< HEAD
-
-    private final long id;
-    private final String content;
-
-    public Greeting(long id, String content){
-=======
     private final long id;
     private final String content;
 
     public Greeting(long id, String content) {
->>>>>>> github/backUnderTest
         this.id = id;
         this.content = content;
     }
 
-<<<<<<< HEAD
-    public long getId(){
-        return id;
-    }
-
-    public String getContent(){
-        return content;
-    }
-
-=======
     public long getId() {
         return id;
     }
@@ -34,5 +16,4 @@ public class Greeting {
     public String getContent() {
         return content;
     }
->>>>>>> github/backUnderTest
 }
