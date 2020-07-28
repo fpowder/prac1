143bd8561b68d24a8894aeb57ca11a57b8abdc5c
kneelick <fpowder1@gmail.com>
2020-07-28 14:17:46 +0900

    add page move

--- src/main/java/com/example/prac1/Prac1Application.java
+++ src/main/java/com/example/prac1/Prac1Application.java
@@ -1,15 +1,12 @@
 package com.example.prac1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.session.config.annotation.web.server.SpringWebSessionConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class,
         DataSourceAutoConfiguration.class,
--- src/main/java/com/example/prac1/Service.java
+++ src/main/java/com/example/prac1/Service.java
@@ -1,15 +1,17 @@
 package com.example.prac1;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Service {
 
-    @GetMapping("/")
-    public String index(){
+    @RequestMapping(value="/")
+    public String index(Model model){
 
-        return "index";
+
+        return "test";
     }
 
 }
